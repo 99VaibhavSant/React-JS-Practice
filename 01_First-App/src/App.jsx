e174378b220c81e026a54e723daa5babc618f663
99vaibhavsant <99vaibhavsant@gmail.com>
2026-01-12 17:30:38 +0530

    Create initial React App with heading and paragraph

---
+++ 01_First-App/src/App.jsx
@@ -0,0 +1,13 @@
+// Initial React App component
+// Displays a simple heading and paragraph
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React 👋</h1>
+      <p>This is my first React application.</p>
+    </div>
+  )
+}
+
+export default App
